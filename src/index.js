62e062e6ca8b967e9e9d3b6926988ec926e571f4
VolhaF <Volha.f@outlook.com>
2023-02-22 12:32:40 +0100

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort (matrix = []) {
+  const columns = matrix.map((arguments, i) => {
+  if ((i + 1) % 2 === 0) {
+    return arguments.reverse();
+  } else {return arguments}
+})
+  return columns.flat();
 }
